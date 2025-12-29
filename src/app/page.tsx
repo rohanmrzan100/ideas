26e4ee8ae6af0683fbb85b52f4f537bff10c155a
PrasanKC <prasankc2000@gmail.com>
2025-12-29 18:30:07 +0545

    personal info form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,23 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="min-h-screen p-10">
-      <h1 className="text-4xl font-bold mb-8">Admin Dashboard / Product List</h1>
+      <h1 className="text-4xl font-bold mb-8">
+        Admin Dashboard / Product List
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <Carousel />
 
         {products.map((product) => (
           <Link
             key={product.description}
-            href={`/product/${product.name.toLowerCase().replace(/ /g, '-')}/products-page`}
+            href={`/product/${product.name.toLowerCase().replace(/ /g, '-')}`}
             className="block p-6 border rounded-lg hover:shadow-lg transition bg-white"
           >
             <h2 className="text-xl font-bold text-black">{product.name}</h2>
             <p className="text-gray-600 mt-2">Rs. {product.price}</p>
-            <p className="text-blue-500 mt-4 text-sm">Click to view unique URL</p>
+            <p className="text-blue-500 mt-4 text-sm">
+              Click to view unique URL
+            </p>
           </Link>
         ))}
       </div>
--- src/components/PersonalInfoForm.tsx
+++ src/components/PersonalInfoForm.tsx
@@ -19,22 +19,33 @@ import {
 
 const ProductInfoForm = () => {
   return (
-    <div className="p-10 flex flex-col min-h-screen">
+    <div className="p-7 flex flex-col min-h-screen">
       <form>
         <FieldSet>
-          <FieldLegend>Personal Info</FieldLegend>
+          <h1 className="text-3xl font-medium border-b-3 border-black py-2">
+            Personal Info
+          </h1>
           <FieldGroup>
             <Field>
-              <FieldLabel htmlFor="name">Full name</FieldLabel>
+              <FieldLabel htmlFor="name" className="text-lg ml-0.5 -mb-1.5">
+                Full name
+              </FieldLabel>
               <Input id="name" autoComplete="off" />
             </Field>
             <Field>
-              <FieldLabel htmlFor="phone-number">Phone Number</FieldLabel>
-              <Input id="username" autoComplete="off" />
-              <FieldDescription>We will sms / call you to confirm your order</FieldDescription>
+              <FieldLabel
+                htmlFor="phone-number"
+                className="text-lg ml-0.5 -mb-2.5"
+              >
+                Phone Number
+              </FieldLabel>
+              <Input id="username" autoComplete="off" className="-mb-1.5" />
+              <FieldDescription className="ml-1.5 italic">
+                We will sms / call you to confirm your order
+              </FieldDescription>
             </Field>
-            <FieldLabel>Address</FieldLabel>
-            <div className="grid grid-cols-2 gap-4">
+            <FieldLabel className="text-lg ml-0.5 -mb-5.5">Address</FieldLabel>
+            <div className="grid grid-cols-2 gap-4 -mb-3">
               <Field>
                 <Select>
                   <SelectTrigger>
@@ -54,8 +65,10 @@ const ProductInfoForm = () => {
               </Field>
             </div>
             <Field>
-              <Input placeholder="Landmark... (Optional)" />
-              <FieldDescription>We will deliver the order to this address</FieldDescription>
+              <Input placeholder="Landmark... (Optional)" className="-mb-2" />
+              <FieldDescription className="ml-1.5 italic">
+                We will deliver the order to this address
+              </FieldDescription>
             </Field>
           </FieldGroup>
         </FieldSet>
@@ -63,13 +76,13 @@ const ProductInfoForm = () => {
       <div className="text-black flex justify-between mt-auto">
         <button
           type="button"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="flex items-center bg-gray-500 hover:bg-gray-600 text-xl text-white py-2 pl-6 pr-6 rounded-sm"
         >
           Back
         </button>
         <button
           type="button"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="flex items-center bg-blue-700 hover:bg-amber-800 text-xl text-white py-2 pl-6 pr-6 rounded-sm"
         >
           Next
         </button>
